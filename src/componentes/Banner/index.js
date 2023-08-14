012d578c3f0f3484200ffd82401068b0100c0c58
Gustavo Henrique L. M. Sousa <130600406+GusOTavo@users.noreply.github.com>
2023-08-13 21:37:39 -0300

    Add files via upload

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+const Banner = () => {
+    //JSX
+    return (
+        <header className="banner">
+            <img src="/img/banner.png" alt="O banner principal da página Organo."/>
+        </header>
+        
+    )
+};
+
+export default Banner;
